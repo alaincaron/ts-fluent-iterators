fe1a4712eea3140169477b1632465c02a5898a01
Alain Caron <caron.alain@gmail.com>
2024-01-11 10:44:21 -0500

    Using typedocs

--- src/lib/sync/generators.ts
+++ src/lib/sync/generators.ts
@@ -1,6 +1,13 @@
 import * as Iterators from './iterators';
 import { Mapper } from '../types';
 
+/**
+   Returns an iterator from `start` (inclusively) to `end` (exclusively) by increment of `step`.
+   @param start the start of the range.  Defaults to 0.
+   @param end the end of the range. Defaults to infinity.
+   @param step increment in the range. Defaults to 1 if `end` > `start`, -1 otherwise.
+   @returns A new iterator from `start` to `end` by increment of `step`.
+   */
 export function* range(start?: number, end?: number, step?: number): IterableIterator<number> {
   if (step === 0) {
     throw new Error(`Invalid value for step: ${step}`);
